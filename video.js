7807d224fa1ef392d06bb8c90d6dc0012518f7f0
LIÊN MÕM 🇻🇳 <159420588+liengamingtool@users.noreply.github.com>
2024-12-13 07:51:52 +0700

    Add video.js

---
+++ video.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('notification').style.display = 'none';
+});
+
+document.getElementById('detailsButton').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=0xAW6MAT_Wo&t=1s'; // URL của video chi tiết
+});

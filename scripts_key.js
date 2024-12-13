6ea2b2b07cd3da30c4941fd5e03a2fcf61c1a1f8
LIÊN MÕM 🇻🇳 <159420588+liengamingtool@users.noreply.github.com>
2024-12-13 07:51:10 +0700

    Add scripts_key.js

---
+++ scripts_key.js
@@ -0,0 +1,36 @@
+document.getElementById('notifyButton').addEventListener('click', function() {
+    const notificationDiv = document.getElementById('notification');
+    notificationDiv.textContent = 'Vui lòng liên hệ chăm sóc khách hàng';
+    notificationDiv.style.display = 'block'; // Show the notification
+        // Hide the notification after 5 seconds
+        setTimeout(function() {
+            notificationDiv.style.display = 'none';
+        }, 3000);
+});
+document.getElementById('notifyButton1').addEventListener('click', function() {
+    const notificationDiv = document.getElementById('notification1');
+    notificationDiv.textContent = 'Vui lòng liên hệ chăm sóc khách hàng';
+    notificationDiv.style.display = 'block'; // Show the notification
+        // Hide the notification after 5 seconds
+        setTimeout(function() {
+            notificationDiv.style.display = 'none';
+        }, 3000);
+});
+document.getElementById('notifyButton2').addEventListener('click', function() {
+    const notificationDiv = document.getElementById('notification2');
+    notificationDiv.textContent = 'Vui lòng liên hệ chăm sóc khách hàng';
+    notificationDiv.style.display = 'block'; // Show the notification
+        // Hide the notification after 5 seconds
+        setTimeout(function() {
+            notificationDiv.style.display = 'none';
+        }, 3000);
+});
+document.getElementById('notifyButton3').addEventListener('click', function() {
+    const notificationDiv = document.getElementById('notification3');
+    notificationDiv.textContent = 'Vui lòng liên hệ chăm sóc khách hàng';
+    notificationDiv.style.display = 'block'; // Show the notification
+        // Hide the notification after 5 seconds
+        setTimeout(function() {
+            notificationDiv.style.display = 'none';
+        }, 3000);
+});
